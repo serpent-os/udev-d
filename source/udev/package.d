d6e0c2d6eced8b5f870e93e4c899e1dafbc7a43c
Ikey Doherty <ikey@serpentos.com>
2023-06-20 10:10:02 +0100

    udev: Add global package
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/udev/package.d
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * udev
+ *
+ * Bindings to libudev
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module udev;
+
+// FIXME: Users should include manually
+import udev.binding;
